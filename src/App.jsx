0fb12318453667dac76bf5e170eeb9f73d5ec811
Develop <sergiohernandez.developer@gmail.com>
2024-11-10 19:11:39 +0100

    Props completadas e itinerados todos los productos

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import Guitar from "./components/Guitar";
 import Header from "./components/Header";
 import { db } from "./data/db";
 
-
 function App() {
-  const [data, setData] = useState(db)
-  
-
+  const [data, setData] = useState(db);
 
   return (
     <>
@@ -17,25 +14,20 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-
-          {data.map((guitar) =>  (
-              <Guitar 
-                guitar={guitar}  
-              />
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} />
           ))}
-      
-        
-
         </div>
       </main>
 
-      <footer className="bg-dark mt-5 py-5" />
-      <div className="container-xl">
-        <p className="text-white text-center fs-4 mt-4 m-md-0">
-          GuitarLA - Todos los derechos Reservados
-        </p>
-      </div>
-      <footer />
+      {/* Footer corregido */}
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,33 +1,43 @@
-function Guitar({guitar}) {
+import PropTypes from 'prop-types';
 
-    
+function Guitar({ guitar }) {
+  // Destructuramos las propiedades del objeto "guitar"
+  const { id, name, image, description, price } = guitar;
 
-
-return (
-
-<div className="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div className="col-4">
-              <img
-                className="img-fluid"
-                src="./public/img/guitarra_01.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div className="col-8">
-              <h3 className="text-black fs-4 fw-bold text-uppercase">
-                {guitar.name}
-              </h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p className="fw-black text-primary fs-3">$</p>
-              <button type="button" className="btn btn-dark w-100">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-)
+  return (
+    <div className="col-md-6 col-lg-4 my-4 d-flex align-items-center">
+      <div className="row w-100">
+        {/* Imagen de la guitarra */}
+        <div className="col-4">
+          <img
+            className="img-fluid"
+            src={`/img/${image}.jpg`}
+            alt={`Imagen de la guitarra ${name}`}
+          />
+        </div>
+        {/* Información de la guitarra */}
+        <div className="col-8">
+          <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+          <p>{description}</p>
+          <p className="fw-bold text-primary fs-3">${price}</p>
+          <button type="button" className="btn btn-dark w-100">
+            Agregar al Carrito
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Guitar
+// Validación de PropTypes para asegurar que recibimos los datos correctos
+Guitar.propTypes = {
+  guitar: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Guitar;

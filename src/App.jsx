a38b403d109b6c950a28021532666dfd7b66e33b
Develop <sergiohernandez.developer@gmail.com>
2024-11-21 22:37:22 +0100

    Limite de productos también en el boton de añadir al carrito y limite mínimo una cantidad de producto

--- src/App.jsx
+++ src/App.jsx
@@ -8,75 +8,70 @@ function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
   const [message, setMessage] = useState("");
-
+  //  Añadir productos al carrito
   function addToCart(item) {
-    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
-    if(itemExists >= 0) { // Existe en el carrito
-      const updateCart = [...cart]
-      updateCart[itemExists].quantity++
-      setCart(updateCart)
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
 
+    if (itemExists >= 0) { // Existe en el carrito
+      if(cart[itemExists].quantity >= quantityMin) return 
+      const updateCart = [...cart];
+      updateCart[itemExists].quantity++;
+      setCart(updateCart);
     } else {
-      item.quantity = 1
-      setCart([...cart, item])
+      item.quantity = 1;
+      setCart([...cart, item]);
+      setMessage(`El producto ${item.name} se añadio al carrito`)
+      cleanMessage()
     }
   }
-
-function removeFromCart(id) {
-  setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
-}
-
-function increaseQuantity(id) {
-  const updateCart = cart.map( item => {
-    if(item.id === id) {
-      const quantityMin = Math.min(item.quantity +1, 5);
-      if(quantityMin === 5) {
-        setMessage(`No puedes agregar más de 5 unidades de ${item.name}`);
-        cleanMessage()
-      } else {
-        setMessage("")
+  // Eliminar productos del carrito
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+  // Incremento de cantidades
+  function increaseQuantity(id) {
+    const updateCart = cart.map((item) => {
+      if (item.id === id) {
+        const quantityMin = Math.min(item.quantity + 1, 5);
+        if (quantityMin === 5) {
+          setMessage(`No puedes agregar más de 5 unidades de ${item.name}`);
+          cleanMessage();
+        } else {
+          setMessage("");
+        }
+        return { ...item, quantity: quantityMin };
       }
-      return {...item, quantity: quantityMin};
-    }
-    return item
-  })
-  setCart(updateCart)
-}
+      return item;
+    });
+    setCart(updateCart);
+  }
+  // Decremento de cantidades
+  function decrementQuantity(id) {
+    const decrementItem = cart.map((item) => {
+      if (item.id === id) {
+        const quantityMax = Math.max(item.quantity - 1, 1);
 
-function decrementQuantity(id) {
-  const decrementItem = cart.map( item => {
-    if(item.id === id) {
-      const quantityMax = Math.max(item.quantity -1, 0);
-      if(quantityMax === 0) {
-        setMessage(`No hay unidades de ${item.name} en el carrito`);
-        cleanMessage()
-      } else {
-        setMessage("")
+        return { ...item, quantity: quantityMax };
       }
-      return {...item, quantity: quantityMax};
-    }
-    return item
-  })
-  setCart(decrementItem)
-}
+      return item;
+    });
+    setCart(decrementItem);
+  }
 
-const cleanMessage = () => {
-  setTimeout(() => {
-    setMessage("")
-  }, 3000);
-}
-
-
-
-
+  // Duración del mensaje y limpieza
+  const cleanMessage = () => {
+    setTimeout(() => {
+      setMessage("");
+    }, 3000);
+  };
   return (
     <>
       <Header
-      cart={cart}
-      removeFromCart={removeFromCart}
-      increaseQuantity={increaseQuantity}
-      decrementQuantity={decrementQuantity}
-      message={message}
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decrementQuantity={decrementQuantity}
+        message={message}
       />
 
       <main className="container-xl mt-5">
@@ -85,10 +80,10 @@ const cleanMessage = () => {
         <div className="row mt-5">
           {data.map((guitar) => (
             <Guitar
-             key={guitar.id} 
-             guitar={guitar} 
-             setCart={setCart} 
-             addToCart={addToCart}
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
             />
           ))}
         </div>
